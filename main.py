04e8b94a10e530aae7b8db6f8e9a9c1d74d9237d
gaultier <gaultier.berriot@gmail.com>
2023-06-09 16:27:53 +0200

    MCFLIRT

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import numpy as np
 import extractgz
 import matplotlib.pyplot as plt
 from fsl.wrappers import bet
+from fsl.wrappers import fslmaths
+import subprocess
+
 
 # https://socr.umich.edu/HTML5/BrainViewer/
 
@@ -58,27 +61,82 @@ def mean_intensity(output_files_Cyberball):
 
 def BET(folders, output_files_T1w):
     for folder, path_file in zip(folders, output_files_T1w):
-        filename = path_file.replace(folder, "")[1:]
-        output_image = folder + "BET" + path_file.replace(folder, "")
+        output_image = "/mnt/c/Users/gault/PycharmProjects/frmi_bpd/" + folder + "/BET" + path_file.replace(folder, "")
         os.makedirs(folder, exist_ok=True)  # Créer le dossier s'il n'existe pas déjà
         # Paramètres BET
         frac = 0.5  # Fraction d'intensité pour BET (0.1 à 0.9)
         # Exécuter BET
         print(output_image)
         bet_cmd = bet(path_file, output_image)
-        bet_cmd.inputs.in_file = path_file
-        bet_cmd.inputs.out_file = output_image
-        bet_cmd.run()
+        print("Le cerveau a été extrait avec succès et enregistré dans : {}".format(output_image))
 
-    print("Le cerveau a été extrait avec succès et enregistré dans : {}".format(output_image))
+
+def run_mcflirt(folders, output_files_T1w):
+    for folder, path_file in zip(folders, output_files_T1w):
+        output_image = "/mnt/c/Users/gault/PycharmProjects/frmi_bpd/" + folder + "/MCFLIRT" + path_file.replace(folder, "")
+        folder = os.path.dirname(output_image)
+        os.makedirs(folder, exist_ok=True)  # Create the folder if it doesn't exist
+        # Execute MCFLIRT
+        command = ['mcflirt', '-in', path_file, '-out', output_image]
+        subprocess.run(command)
+        print("MCFLIRT successfully applied and saved the output to: {}".format(output_image))
+
+
+def sclice_timing_correction(input_files, folders):
+    tr = 3.0  # Temps de répétition (TR) en secondes
+    ta = 1.0  # Temps d'acquisition (TA) de chaque tranche en secondes
+    # num_slices = 30  # Nombre de tranches dans vos images IRMF
+    interleaved = True  # True si vos tranches sont acquises de manière entrelacée, False sinon
+
+    # Parcourez chaque fichier d'IRMF et appliquez la correction de synchronisation des tranches
+    for input_file, output_folder in zip(input_files, folders):
+        output_path = output_folder + "/SCLICE_TC" + input_file.replace(output_folder, "")
+        os.makedirs(output_path, exist_ok=True)  # Create the folder if it doesn't exist
+        # Construisez la commande FSL pour la correction de synchronisation des tranches
+        cmd = f"slicetimer -i {input_file} -o {output_path} --odd --ocustom={output_path} --tcustom={tr} --repeat={ta}"
+        if interleaved:
+            cmd += " --odd"
+
+        # Exécutez la commande FSL
+        subprocess.call(cmd, shell=True)
+
+        print(f"Correction de synchronisation des tranches terminée pour {input_file}")
+
+    print("Toutes les corrections de synchronisation des tranches sont terminées.")
+
+
+def spacial_smoothing(input_files, folders):
+    for input_file, output_folder in zip(input_files, folders):
+        # Nom du fichier de sortie
+        output_path = output_folder + "/SCLICE_TC" + input_file.replace(output_folder, "")
+        output_file = "image_smoothed.nii.gz"
+
+        # Taille de la fenêtre de lissage (en mm)
+        smoothing_size = 5
+
+        # Commande de lissage spatial avec FSL
+        # fsl_cmd = f"fslmaths {os.path.join(data_folder, input_file)} -s {smoothing_size} {os.path.join(data_folder, output_file)}"
+
+        # Exécution de la commande
+        # subprocess.call(fsl_cmd, shell=True)
 
 
 if __name__ == "__main__":
-    output_files_Cyberball, output_files_T1w, output_folders = extractgz.get_list_filename()
-    print(output_folders, output_files_T1w)
-    BET(output_folders, output_files_T1w)
+    # TODO analyse shape pour savoir qui est le IRM et l'IRMF
+    # Refaire les traitements sur le shape de 4
 
+    output_files_Cyberball, output_files_T1w, folders = extractgz.get_list_filename()
+    print(output_files_Cyberball)
+    print(output_files_T1w)
+    print(folders)
+    # BET(output_folders, output_files_T1w)
+    # run_mcflirt(output_folders, output_files_T1w)
+    sclice_timing_correction(output_files_T1w, folders)
 
 # https://github.com/Project-MONAI/tutorials/blob/main/3d_segmentation/brats_segmentation_3d.ipynb
 # https://neuraldatascience.io/8-mri/nifti.html
 # https://medium.com/miccai-educational-initiative/how-to-get-started-with-deep-learning-using-mri-data-5d6a41dbc417
+
+
+# export FSLDIR=/usr/local/fsl
+# source /usr/local/fsl/etc/fslconf/fsl.sh
